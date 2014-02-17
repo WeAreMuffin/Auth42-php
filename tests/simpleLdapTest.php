c1f2a1edfbce502bb49968f36272146f6ad3a2a2
André Aubin <andre.aubin@lambdaweb.fr>
2014-02-17 16:56:14 +0100

    Adding search function

--- tests/simpleLdapTest.php
+++ tests/simpleLdapTest.php
@@ -7,30 +7,31 @@ require_once '../Auth42.php';
  * @author sanecz <lbekdach@student.42.fr>
  */
 
-$user_login = "";
-$user_password = "";
+$login = "";
+$password = "";
 
-$ldaprdn = 'uid=' . $user_login . ',ou=2013,ou=people,dc=42,dc=fr'; // couple user_login user_password que tu as recup de quelconque maniere..
-$ldappass = $user_password;
-
-$ldapconn = ldap_connect ("ldaps://ldap.42.fr") or die ("Could not connect to LDAP server.");
-ldap_set_option ($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-if ( $ldapconn )
-{
-    $ldapbind = @ldap_bind ($ldapconn, $ldaprdn, $ldappass);
-    if ( $ldapbind )
-        echo "LDAP bind successful...\n"; // Si tu peux bind, c'est que le couple pass/login est correct, sinon, non..
-    else
-        echo "LDAP bind failed...\n";
-}
-ldap_close ($ldapconn);
 
 /* Trying with Auth42 wrapper */
-$result = Auth42::authenticate ($user_login, $user_password);
-    if ( $result )
-        echo "authenticate bind successful...\n"; // Si tu peux bind, c'est que le couple pass/login est correct, sinon, non..
-    else
-        echo "authenticate bind failed...\n";
 
+$auth = new Auth42();
+$auth->setPassword ($password);
+$auth->setDn (array (
+    "uid" => $login,
+    "ou" => array ("2013", "people"),
+    "dc" => array ("42", "fr")
+));
+
+try
+{
+	$info = $auth->search("uid=sbarale");
+	    for ($i=0; $i<$info["count"]; $i++) {
+	    	var_dump($info);
+	    }
+
+    ldap_close($ds);
+}
+catch (Exception $e)
+{
+    echo "Une erreur s'est produite : ".$e->getMessage();
+}
 ?>
